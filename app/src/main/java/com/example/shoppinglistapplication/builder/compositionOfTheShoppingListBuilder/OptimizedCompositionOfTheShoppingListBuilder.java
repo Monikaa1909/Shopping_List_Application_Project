296da8efacee7dc0f0ca15ce5d0c8e0bbcad953c
Jakub Zdanowicz <jakub.zdanowicz99@gmail.com>
2022-01-23 14:04:27 +0100

    algorytm v2

--- app/src/main/java/com/example/shoppinglistapplication/builder/compositionOfTheShoppingListBuilder/OptimizedCompositionOfTheShoppingListBuilder.java
+++ app/src/main/java/com/example/shoppinglistapplication/builder/compositionOfTheShoppingListBuilder/OptimizedCompositionOfTheShoppingListBuilder.java
@@ -47,37 +47,60 @@ public class OptimizedCompositionOfTheShoppingListBuilder implements ICompositio
                 s++;
             }
 
+            int leastAmount;
+            double smallestReminder;
             boolean firstAppend = true;
-            StringBuilder stringBuilder = new StringBuilder();
             int i = 0;
+
+            List<Double> calculatedReminders = new ArrayList<>();
+            StringBuilder stringBuilder = new StringBuilder();
+
+
+            leastAmount = calculateAmount(weight, packageSizes[0]);
+            calculatedReminders.add(calculateRemainder(weight, packageSizes[0], leastAmount));
+            smallestReminder = calculatedReminders.get(0);
             while (weight > 0) {
 
                 if (isLastIndex(packageSizes, i)) {
                     int amount = calculateAmount(weight, packageSizes[i]);
                     append(packageSizes[i], firstAppend, stringBuilder, amount);
-                    weight = updateWeight(weight, packageSizes[i] * amount);
+                    weight -= updateWeight(weight, packageSizes[i] * amount);
                 } else {
-                    List<Double> calculatedReminders = new ArrayList<>();
-                    calculatedReminders.add(calculateRemainder(weight, packageSizes[0], calculateAmount(weight, packageSizes[0])));
-                    double minReminder = calculatedReminders.get(0);
-                    int minReminderIndex = 0;
-                    for (int j = 1; j < packageSizes.length; j++) {
-                        double reminder = calculateRemainder(weight, packageSizes[j], calculateAmount(weight, packageSizes[j]));
+                    int smallestReminderIndex = 0;
+                    for (int j = 0; j < packageSizes.length; j++) {
+                        int amount = calculateAmount(weight, packageSizes[j]);
+                        double reminder = calculateRemainder(weight, packageSizes[j], amount);
                         if (reminder == 0) {
-                            minReminder = reminder;
-                            minReminderIndex = j;
-                        } else if (reminder > minReminder) {
-                            minReminder = reminder;
-                            minReminderIndex = j;
+                            weight -= updateWeight(packageSizes[j], amount);
+                            append(packageSizes[j], firstAppend, stringBuilder, amount);
+                            continue;
+                        } else if (reminder < 0) {
+                            if (j != packageSizes.length - 1) {
+                                int amountOfSmallerPackage = calculateAmount(weight, packageSizes[j + 1]);
+                                if (Math.abs(reminder) > Math.abs(calculateRemainder(weight, packageSizes[j + 1], amountOfSmallerPackage)))
+                                    amount--;
+                            }
+                            if (amount <= 0) {
+                                continue;
+                            } else {
+                                weight -= updateWeight(packageSizes[j], amount);
+                                append(packageSizes[j], firstAppend, stringBuilder, amount);
+                                stringBuilder.append("+");
+                            }
+                        } else if (reminder < smallestReminder || (reminder == smallestReminder && leastAmount > amount)) {
+                            smallestReminder = reminder;
+                            leastAmount = amount;
+                            smallestReminderIndex = j;
                         }
                         calculatedReminders.add(reminder);
                     }
-                    int amount = calculateAmount(weight, packageSizes[minReminderIndex]);
-                    append(packageSizes[minReminderIndex], firstAppend, stringBuilder, amount);
-                    weight = updateWeight(weight, amount * packageSizes[minReminderIndex]);
+                    int amount = calculateAmount(weight, packageSizes[smallestReminderIndex]);
+
+                    weight -= updateWeight(weight, amount * packageSizes[smallestReminderIndex]);
                 }
             }
-            optimizedCompositionOfTheShoppingList.setSuggestedFormOfAccessibility(String.valueOf(stringBuilder) + " " + unit.toUpperCase());
+
+            optimizedCompositionOfTheShoppingList.setSuggestedFormOfAccessibility(optimizedPackagingToString(stringBuilder) + " " + unit.toUpperCase());
         } else {
             optimizedCompositionOfTheShoppingList.setSuggestedFormOfAccessibility("Brak danych");
         }
@@ -90,8 +113,9 @@ public class OptimizedCompositionOfTheShoppingListBuilder implements ICompositio
     }
 
 
+
     private static double updateWeight(double weight, double amount) {
-        return weight - amount;
+        return weight * amount;
     }
 
     private static void append(double packageSize, boolean firstAppend, StringBuilder stringBuilder, int amount) {
@@ -114,5 +138,11 @@ public class OptimizedCompositionOfTheShoppingListBuilder implements ICompositio
     }
     static double calculateRemainder(double weight, double packageSize, int amount) {
         return weight - packageSize * amount;
+    }
+
+    public static StringBuilder optimizedPackagingToString(StringBuilder stringBuilder) {
+        String packaging = stringBuilder.toString();
+        String outcome = packaging.substring(0, packaging.length() - 1);
+        return new StringBuilder(outcome);
     }
 }
